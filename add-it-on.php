19a1f648d87652e64891be42166e47ce6ee31e30
MarcoValeri <marcovaleri@hotmail.it>
2022-10-17 10:15:13 +0100

    First Commit: set up the project

---
+++ add-it-on.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Add it on header, footer and body
+ * Description: WordPress plugin that allows to add your code on the header, footer and body section
+ * Version: 1.0.0
+ * Request at least: WordPress 6.0
+ * Requires PHP: 8
+ * Author: Marco Valeri
+ * Author URI: https://www.devmarco.com/
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: add-it-on
+ * Domain Path: /languages
+ */
